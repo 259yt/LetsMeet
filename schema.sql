c635b7e00390f9116c7662d0225b76696b4784a4
Yannick Thomke <yat@abatgroup.de>
2025-08-27 11:32:50 +0200

    user -> app_user

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 -- Tabelle für die Hauptbenutzerinformationen
-CREATE TABLE user (
-                      user_email VARCHAR(255) PRIMARY KEY,
-                      first_name VARCHAR(255),
-                      last_name VARCHAR(255),
-                      gender VARCHAR(20),
-                      date_of_birth DATE
+CREATE TABLE app_user (
+                          user_email VARCHAR(255) PRIMARY KEY,
+                          first_name VARCHAR(255),
+                          last_name VARCHAR(255),
+                          gender VARCHAR(20),
+                          date_of_birth DATE
 );
 
 ---
@@ -12,7 +12,7 @@ CREATE TABLE user (
 -- Tabelle für Adressinformationen
 CREATE TABLE address (
                          address_id INT PRIMARY KEY,
-                         user_email VARCHAR(255) REFERENCES user(user_email),
+                         user_email VARCHAR(255) REFERENCES app_user(user_email),
                          street VARCHAR(255),
                          house_number VARCHAR(50),
                          city VARCHAR(255),
@@ -24,7 +24,7 @@ CREATE TABLE address (
 -- Tabelle für Telefonnummern
 CREATE TABLE phone_number (
                               phone_number VARCHAR(50) PRIMARY KEY,
-                              user_email VARCHAR(255) REFERENCES user(user_email)
+                              user_email VARCHAR(255) REFERENCES app_user(user_email)
 );
 
 ---
@@ -32,7 +32,7 @@ CREATE TABLE phone_number (
 -- Tabelle für Fotos
 CREATE TABLE photo (
                        photo_id INT PRIMARY KEY,
-                       user_email VARCHAR(255) REFERENCES user(user_email),
+                       user_email VARCHAR(255) REFERENCES app_user(user_email),
                        date DATE,
                        is_profile_picture BOOLEAN
 );
@@ -65,7 +65,7 @@ CREATE TABLE conversation (
 
 -- Zwischentabelle zur Verknüpfung von Benutzern und Hobbys
 CREATE TABLE user_hobby (
-                            user_email VARCHAR(255) REFERENCES user(user_email),
+                            user_email VARCHAR(255) REFERENCES app_user(user_email),
                             hobby_id INT REFERENCES hobby(hobby_id),
                             priority INT,
                             PRIMARY KEY (user_email, hobby_id)
@@ -76,8 +76,8 @@ CREATE TABLE user_hobby (
 -- Zwischentabelle für die Freundschaftsbeziehung zwischen zwei Benutzern
 CREATE TABLE friendship (
                             friendship_id INT PRIMARY KEY,
-                            user_id_1 VARCHAR(255) REFERENCES user(user_email),
-                            user_id_2 VARCHAR(255) REFERENCES user(user_email),
+                            user_id_1 VARCHAR(255) REFERENCES app_user(user_email),
+                            user_id_2 VARCHAR(255) REFERENCES app_user(user_email),
                             status VARCHAR(50),
                             date_of_change DATE,
                             date_of_creation DATE
@@ -88,8 +88,8 @@ CREATE TABLE friendship (
 -- Zwischentabelle für Interessen (Likes)
 CREATE TABLE interest (
                           interest_id INT PRIMARY KEY,
-                          from_user_id VARCHAR(255) REFERENCES user(user_email),
-                          to_user_id VARCHAR(255) REFERENCES user(user_email),
+                          from_user_id VARCHAR(255) REFERENCES app_user(user_email),
+                          to_user_id VARCHAR(255) REFERENCES app_user(user_email),
                           date_of_change DATE,
                           date_of_creation DATE
 );
@@ -98,7 +98,7 @@ CREATE TABLE interest (
 
 -- Zwischentabelle für die Geschlechtsinteressen
 CREATE TABLE gender_interest (
-                                 user_email VARCHAR(255) REFERENCES user(user_email),
+                                 user_email VARCHAR(255) REFERENCES app_user(user_email),
                                  gender_id INT REFERENCES gender(gender_id),
                                  PRIMARY KEY (user_email, gender_id)
 );
@@ -109,8 +109,8 @@ CREATE TABLE gender_interest (
 CREATE TABLE message (
                          message_id INT PRIMARY KEY,
                          conversation_id INT REFERENCES conversation(conversation_id),
-                         sender_id VARCHAR(255) REFERENCES user(user_email),
-                         receiver_id VARCHAR(255) REFERENCES user(user_email),
+                         sender_id VARCHAR(255) REFERENCES app_user(user_email),
+                         receiver_id VARCHAR(255) REFERENCES app_user(user_email),
                          text TEXT,
                          date_of_sending DATE
 );
